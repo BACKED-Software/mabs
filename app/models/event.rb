d4965a0da12e06346044751eef0b5f7079ec469a
root <fornerobj@gmail.com>
2024-02-06 00:12:12 +0000

    created events model and database table

---
+++ app/models/event.rb
@@ -0,0 +1,6 @@
+class Event < ApplicationRecord
+    #will be added later after sponsor table is created
+    #belongs_to :sponsor
+  
+    # Add any additional associations or validations here
+  end
---
+++ db/migrate/20240206000557_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.text :eventLocation
+      t.text :eventInfo
+      t.string :eventName
+      t.datetime :eventTime
+
+      t.timestamps
+    end
+  end
+end
