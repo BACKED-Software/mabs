8253a204a1fd577c2d1ac362333c8bdeb0afefc5
root <fornerobj@gmail.com>
2024-02-06 00:55:22 +0000

    unit tests passing

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,8 @@ class Event < ApplicationRecord
     #belongs_to :sponsor
   
     # Add any additional associations or validations here
-    
+    validates :eventInfo, presence: true
+    validates :eventName, presence: true
+    validates :eventTime, presence: true
+    validates :eventLocation, presence: true
   end
