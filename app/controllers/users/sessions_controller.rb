3f18beaa28e6ec3d55942ed19ebc29c5185e7ec9
BjDaMaster <42049727+BjDaMaster@users.noreply.github.com>
2024-02-21 19:03:41 +0000

    Sign out reroutes to root

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@
 module Users
   class SessionsController < Devise::SessionsController
     def after_sign_out_path_for(_resource_or_scope)
-      new_user_session_path
+      root_path
     end
 
     def after_sign_in_path_for(resource_or_scope)
