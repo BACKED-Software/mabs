ba987973ad9b0fd7ce2dda207dfa46f5a0e28afa
root <root@localhost>
2024-02-29 18:47:09 +0000

    Added attendances table

---
+++ db/migrate/20240229162704_create_attendances.rb
@@ -0,0 +1,12 @@
+class CreateAttendances < ActiveRecord::Migration[7.0]
+  def change
+    create_table :attendances do |t|
+      t.integer :eventID, foreign_key: true
+      t.text :googleUserID, foreign_key: true
+      t.datetime :timeOfCheckIn
+      t.integer :pointsAwarded
+
+      t.timestamps
+    end
+  end
+end
