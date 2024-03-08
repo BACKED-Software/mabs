95c5f34924c55dfeecb1a5ba0f8c69a62a53f7c1
BjDaMaster <42049727+BjDaMaster@users.noreply.github.com>
2024-03-08 23:24:34 +0000

    all rainy and sunny day tests passing

--- spec/features/recent_points_spec.rb
+++ spec/features/recent_points_spec.rb
@@ -1,22 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "User's history", type: :feature do
-  let(:setup) { create(:setup) }
 
   before do
-    login_as(setup.user)
-    visit root_path
+    @user = create(:user)
+    sign_in @user
+    @event = Event.create(
+      eventLocation: 'Sample Location',
+      eventInfo: 'Sample Info',
+      eventName: 'Sample Event',
+      eventTime: DateTime.now,
+      sponsor_title: 'Sample Title',
+      sponsor_description: 'Sample Description'
+    )
+    @point = Point.create(
+      awardedBy: @user.uid,
+      awardedTo: @user.uid,
+      numPointsAwarded: 10,
+      awardDescription: 'Test Point',
+      dateOfAward: DateTime.now
+    )
+    @attendance = Attendance.create(
+      googleUserID: @user.uid,
+      eventID: @event.id,
+      timeOfCheckIn: DateTime.now,
+      pointsAwarded: 10
+    )
   end
 
   it 'shows the most recent point' do
-    expect(page).to have_content(setup.point.awardDescription)
-    expect(page).to have_content(setup.point.numPointsAwarded)
+    visit root_path
+    expect(page).to have_content(@point.awardDescription)
+    expect(page).to have_content(@point.numPointsAwarded)
   end
 
   it 'shows the most recent attendance' do
-    expect(page).to have_content(setup.attendance.created_at.strftime("%m/%y"))
-    expect(page).to have_content("Attendance to #{setup.event.eventName}")
-    expect(page).to have_content(setup.attendance.pointsAwarded)
+    visit root_path
+    expect(page).to have_content(@attendance.created_at.strftime("%m/%y"))
+    expect(page).to have_content("Attendance to #{@attendance.event.eventName}")
+    expect(page).to have_content(@attendance.pointsAwarded)
   end
 
   context 'when there is no point history' do
@@ -26,7 +48,7 @@ RSpec.describe "User's history", type: :feature do
     end
 
     it 'does not show any points' do
-      expect(page).not_to have_content('Points')
+      expect(page).not_to have_content(@point.awardDescription)
     end
   end
 
@@ -35,9 +57,39 @@ RSpec.describe "User's history", type: :feature do
       Attendance.delete_all
       visit root_path
     end
-
+    
     it 'does not show any attendances' do
-      expect(page).not_to have_content('Attendances')
+      within('.pointHistory') do
+        expect(page).not_to have_content(@attendance.event.eventName)
+      end
+    end
+  end
+
+  context 'when there are points but no attendance' do
+    before do
+      Attendance.delete_all
+      visit root_path
+    end
+  
+    it 'shows points but not attendances' do
+      within('.pointHistory') do
+        expect(page).to have_content(@point.awardDescription)
+        expect(page).not_to have_content(@attendance.event.eventName)
+      end
+    end
+  end
+  
+  context 'when there is attendance but no points' do
+    before do
+      Point.delete_all
+      visit root_path
+    end
+  
+    it 'shows attendances but not points' do
+      within('.pointHistory') do
+        expect(page).to have_content(@attendance.event.eventName)
+        expect(page).not_to have_content(@point.awardDescription)
+      end
     end
   end
 end
