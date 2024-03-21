fefbac214760752f72ad75ed9d8cc87e7484dbf0
“kirk-jgraham3” <kirk.jgraham3@tamu.edu>
2024-03-21 03:53:38 -0500

    Fixed Rspec

--- spec/features/leaderboard_spec.rb
+++ spec/features/leaderboard_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Leaderboard', type: :feature do
 
   it 'displays the correct point total' do
     visit leaderboard_index_path
-    expect(page).to have_content("#{user1.total_points} points")
+    expect(page).to have_content("#{user1.total_points}")
   end
 
   it 'displays points in descending order' do
@@ -36,24 +36,28 @@ RSpec.describe 'Leaderboard', type: :feature do
     expect(users_after_top_users).to match(/#{user2.full_name}.*#{user1.full_name}/m)
   end
 
-  it 'displays the current_user rank' do
+  it 'displays the current user rank with appropriate medal icon' do
     visit leaderboard_index_path
-    expect(page).to have_content('Your Ranking: 2', count: 1)
+
+    within('.current-user-rank') do
+      expect(page).to have_css("h4.rank i.fas.fa-medal[style='color: goldenrod;']", count: 1) if @current_user_rank == 1
+      expect(page).to have_css("h4.rank i.fas.fa-medal[style='color: silver;']", count: 1) if @current_user_rank == 2
+      expect(page).to have_css("h4.rank i.fas.fa-medal[style='color: #CD7F32;']", count: 1) if @current_user_rank == 3
+      expect(page).to have_content(@current_user_rank) # Make sure the rank number is present
+    end
   end
 
-  it 'displays all users rankings' do
+  it 'displays all users ranking trophies' do
     visit leaderboard_index_path
     click_link 'View All Users'
-    expect(page).to have_content('#1', count: 1)
-    expect(page).to have_content('#2', count: 1)
-    expect(page).to have_content('#3', count: 1)
-    expect(page).to have_content('#4', count: 2)
+    expect(page).to have_css("i.fas.fa-medal", count: 4) # Make sure four medal icons are present (gold, silver, bronze)
+    expect(page).to have_content('#', count: 2)
   end
 
   it 'gives tied users the same rank' do
     visit leaderboard_index_path
     click_link 'View All Users'
-    expect(page).to have_content('#4', count: 2)
+    expect(page).to have_content('4', count: 2)
   end
 
   it 'allows viewing all users' do
@@ -67,19 +71,19 @@ RSpec.describe 'Leaderboard', type: :feature do
 
   it 'allows viewing next users' do
     visit leaderboard_index_path
-    expect(page).to have_content(user1.full_name, count: 2) # Will find twice since user name is also in off-canvas for profile, user1 is displayed by default since they are logged in
+    expect(page).to have_content(user1.full_name)
     expect(page).to have_content(user2.full_name) # user2 has most points
     expect(page).not_to have_content(user3.full_name)
     expect(page).not_to have_content(user4.full_name)
 
     click_link 'View Next Users'
-    expect(page).to have_content(user1.full_name, count: 3) # user1 should appear three times now as they are off-canvas, on the left, and on the right
+    expect(page).to have_content(user1.full_name, count: 2)
     expect(page).to have_content(user2.full_name)
     expect(page).not_to have_content(user3.full_name)
     expect(page).not_to have_content(user4.full_name)
 
     click_link 'View Next Users'
-    expect(page).to have_content(user1.full_name, count: 3) # user1 should appear three times now as they are off-canvas, on the left, and on the right
+    expect(page).to have_content(user1.full_name, count: 2)
     expect(page).to have_content(user2.full_name)
     expect(page).to have_content(user3.full_name)
     expect(page).not_to have_content(user4.full_name)
