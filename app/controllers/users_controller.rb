f82799e6d10abd5a79f0988ec3cbea65d29b6974
dineshba25 <dineshba63@gmail.com>
2024-03-24 20:41:31 -0500

    fixed url direction of edit user profile submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to(@user, notice: 'User was successfully updated.') }
+        format.html { redirect_to(dashboard_index_path, notice: 'Your profile information was successfully updated.') }
         format.json { render(:show, status: :ok, location: @user) }
       else
         format.html { render(:edit, status: :unprocessable_entity) }
