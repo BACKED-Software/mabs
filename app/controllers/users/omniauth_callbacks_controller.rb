cf09cdc26841fb05d3cb8b63739bdcd497ba1232
dineshba25 <dineshba63@gmail.com>
2024-03-24 21:06:43 -0500

    fixed notice color upon login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,10 +7,10 @@ module Users
 
       if user.present?
         sign_out_all_scopes
-        flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+        flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect user, event: :authentication
       else
-        flash[:alert] =
+        flash[:error] =
           t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
         redirect_to new_user_session_path
       end
